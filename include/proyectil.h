e06bb78c6ed813fba90c3edee4ed34e42448775a
ArturoNU <51975879+ArturoNU@users.noreply.github.com>
2019-06-18 19:55:18 -0500

    clase proyectil

---
+++ include/proyectil.h
@@ -0,0 +1,16 @@
+#ifndef PROYECTIL_H
+#define PROYECTIL_H
+
+
+class proyectil
+{
+    public:
+        proyectil();
+        virtual ~proyectil();
+
+    protected:
+
+    private:
+};
+
+#endif // PROYECTIL_H
